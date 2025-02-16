95bbf024f08547207cbae4ca9f7d153fc692d8a8
rafi0x <rafiulawal.r@gmail.com>
2025-02-16 17:04:35 +0600

    read devices from config

---
+++ config.py
@@ -0,0 +1,13 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'database': 'hrm',
+    'user': 'demo',
+    'password': '00',
+    'port': 5432
+}
+
+# List of ZKTeco devices
+DEVICES = [
+    # {'ip': '', 'port': 4370},
+    {'ip': '192.168.0.12', 'port': 4370},
+]
